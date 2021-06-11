607ec3e70b78bb52d7eaf28cd02b660b43d4bd18
Ryan Maas <rdmjobs@live.com>
2021-06-11 16:33:36 -0500

    Initial commit, setup index.js and installed dependencies.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//#region ⬇⬇ Document setup below: 
+// ⬇ index.js & App.js setup: 
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+// ⬇ React/Redux functionality:
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+//#endregion ⬆⬆ Document setup above. 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//#region ⬇⬇ All reducers below: 
+
+//#endregion ⬆⬆ All reducers above. 
+
+
+//#region ⬇⬇ Store & render below: 
+// ⬇ Store: 
+const store = createStore(
+  combineReducers({
+
+  }), applyMiddleware(
+    logger
+  )
+);
+
+// ⬇ Render:
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
+//#endregion ⬆⬆ Store * render above.
