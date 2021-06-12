e64bf5751cb92c5d409d01a872cd4a9b04817140
Ryan Maas <rdmjobs@live.com>
2021-06-12 12:23:41 -0500

    Question1 form done.

--- src/components/Question1/Question1.jsx
+++ src/components/Question1/Question1.jsx
@@ -5,7 +5,7 @@ import './Question1.css';
 import { useSelector, useDispatch } from 'react-redux';
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Select, InputLabel, MenuItem, FormControl, FormHelperText } from '@material-ui/core';
+import { Button, Select, InputLabel, MenuItem, FormControl, FormHelperText, TextField } from '@material-ui/core';
 //#endregion ⬆⬆ Document setup above. 
 
 
@@ -19,35 +19,42 @@ export default function Question1() {
 
   //#region ⬇⬇ Event handlers below:
   const handleSubmit = () => {
-    console.log('In handleSubmit, feeling:', feeling);
-
+    console.log('In Question1 handleSubmit, feeling:', feeling);
+    // ⬇ Sending user input to the reducer:
+    dispatch({
+      type: 'ADD_FEEDBACK',
+      payload: feeling
+    }); // End dispatch
+    // ⬇ Sending user to next page: 
+    history.push('/question2');
   } // End handleSubmit
   //#endregion ⬆⬆ Event handles above. 
 
-  
+
   // ⬇ Rendering:
   return (
     <div className="question1-wrapper">
       <h2>How are you feeling today?</h2>
 
       <div className="question1-form">
-        <FormControl required >
-          <Select
+        <form onSubmit={handleSubmit}>
+          <TextField
             required
+            select
             defaultValue=""
             onChange={event => setFeeling(event.target.value)}
+            helperText="Required"
           >
             <MenuItem value='1'>1</MenuItem>
             <MenuItem value='2'>2</MenuItem>
             <MenuItem value='3'>3</MenuItem>
             <MenuItem value='4'>4</MenuItem>
             <MenuItem value='5'>5</MenuItem>
-          </Select>
-          <FormHelperText>Required</FormHelperText>
-        </FormControl>
-        <Button onClick={handleSubmit}>
-          Next
-        </Button>
+          </TextField>
+          <Button type="submit">
+            Next
+          </Button>
+        </form>
       </div>
 
     </div>
