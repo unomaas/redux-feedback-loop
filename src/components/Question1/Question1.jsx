201cbd37aeea5171bef28d0a4908f66b8421b8b0
Ryan Maas <rdmjobs@live.com>
2021-06-12 11:29:28 -0500

    Working on Question1 form submit.

--- src/components/Question1/Question1.jsx
+++ src/components/Question1/Question1.jsx
@@ -5,17 +5,38 @@ import './Question1.css';
 import { useSelector, useDispatch } from 'react-redux';
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Select, InputLabel, MenuItem, FormControl } from '@material-ui/core';
+import { Button, Select, InputLabel, MenuItem, FormControl, FormHelperText } from '@material-ui/core';
 //#endregion ⬆⬆ Document setup above. 
 
 
 export default function Question1() {
+
+  const [feeling, setFeeling] = useState('');
+
+  const handleSubmit = () => {
+    console.log('In handleSubmit, feeling:', feeling);
+
+  }
+
   return (
-    <div>
+    <div className="question1-wrapper">
       <h2>How are you feeling today?</h2>
 
+      <div className="question1-form">
+        <FormControl required className="form-control" onSubmit={handleSubmit}>
+          <Select onChange={event => setFeeling(event.target.value)}>
+            <MenuItem value='1'>1</MenuItem>
+            <MenuItem value='2'>2</MenuItem>
+            <MenuItem value='3'>3</MenuItem>
+            <MenuItem value='4'>4</MenuItem>
+            <MenuItem value='5'>5</MenuItem>
+          </Select>
+          <Button type="submit">
+            Next
+          </Button>
 
-
+        </FormControl>
+      </div>
       
     </div>
   ) // End return
