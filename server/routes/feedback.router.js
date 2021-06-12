2d84237bf8acae521d243829543feb8d9ced8a5b
Ryan Maas <rdmjobs@live.com>
2021-06-12 09:45:49 -0500

    Added server-side post functionality.

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -10,7 +10,7 @@ const pool = require('../modules/pool');
  * Router function to handle the GET part of the server-side logic.  Will send SQL query to pull all of the tasks from the DB to update on the DOM.
  */
 router.get('/', (req, res) => {
-  console.log('GET /feedback');
+  console.log('In GET /feedback');
   // ⬇ Declaring SQL commands to send to DB: 
   const sqlText = `SELECT * from "feedback";`;
   // ⬇ Sending query to DB:
@@ -24,7 +24,32 @@ router.get('/', (req, res) => {
       console.log('In /feedback GET, error:', error);
       res.sendStatus(500); // Server error. 
     }); // End .catch
-})
+}) // End GET /feedback
+
+/** ⬇ /treats POST functionality:
+ * Router function to handle the POST part of the server-side logic.  Will send SQL query to add new treats to the DB and output to DOM. 
+ */
+ router.post('/', (req, res) => {
+  console.log('In POST /feedback');
+  // ⬇ Saving the req.body as a variable:
+  let newFeedback = req.body;
+  console.log(`Feedback to add is:`, newFeedback);
+  // ⬇ Declaring the SQL commands:
+  const sqlText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);`;
+  // ⬇ Sanitizing values, which need to be sent as an array:
+  const values = [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments]
+  // ⬇ Sending query to DB:
+  pool.query(sqlText, values)
+    .then(result => {
+      console.log('In /feedback POST, response:', res);
+      res.sendStatus(201); // Created. 
+    }) // End .then
+    .catch(error => {
+      console.log(`In /feedback POST, error:`, error);
+      res.sendStatus(500);
+    }); // End .catch
+}); // End POST /feedback 
+//#endregion ⬆⬆ All CRUD routes above. 
 
 
 module.exports = router;
