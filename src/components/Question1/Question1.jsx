d335ce74cc2234ca8a163ce1ffb7b7adc3f83a8a
Ryan Maas <rdmjobs@live.com>
2021-06-12 11:52:19 -0500

    Saving before switching from Select to TextField.

--- src/components/Question1/Question1.jsx
+++ src/components/Question1/Question1.jsx
@@ -10,34 +10,46 @@ import { Button, Select, InputLabel, MenuItem, FormControl, FormHelperText } fro
 
 
 export default function Question1() {
-
+  //#region ⬇⬇ State variables below:
+  const dispatch = useDispatch();
+  const history = useHistory();
   const [feeling, setFeeling] = useState('');
+  //#endregion ⬆⬆ State variables above. 
 
+
+  //#region ⬇⬇ Event handlers below:
   const handleSubmit = () => {
     console.log('In handleSubmit, feeling:', feeling);
 
-  }
+  } // End handleSubmit
+  //#endregion ⬆⬆ Event handles above. 
 
+  
+  // ⬇ Rendering:
   return (
     <div className="question1-wrapper">
       <h2>How are you feeling today?</h2>
 
       <div className="question1-form">
-        <FormControl required className="form-control" onSubmit={handleSubmit}>
-          <Select onChange={event => setFeeling(event.target.value)}>
+        <FormControl required >
+          <Select
+            required
+            defaultValue=""
+            onChange={event => setFeeling(event.target.value)}
+          >
             <MenuItem value='1'>1</MenuItem>
             <MenuItem value='2'>2</MenuItem>
             <MenuItem value='3'>3</MenuItem>
             <MenuItem value='4'>4</MenuItem>
             <MenuItem value='5'>5</MenuItem>
           </Select>
-          <Button type="submit">
-            Next
-          </Button>
-
+          <FormHelperText>Required</FormHelperText>
         </FormControl>
+        <Button onClick={handleSubmit}>
+          Next
+        </Button>
       </div>
-      
+
     </div>
   ) // End return
 } // End Question1
